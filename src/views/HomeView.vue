62c19ea33968ccf06368f045dd53c746262397a9
mohammad sadegh ilaghi <m.hoseini966@gmail.com>
2023-03-13 20:44:18 +0330

    created project and write counter code

---
+++ src/views/HomeView.vue
@@ -0,0 +1,64 @@
+
+<template>
+  <div class="home">
+    <button @click="decreaseCounter" class="btn">-</button> 
+    <span class="counter">{{ counter }}</span>   
+    <button @click="increaseCounter" class="btn">+ </button>
+  </div>
+</template>
+
+<!-- script setup pattern -->
+<script setup>
+import { ref } from 'vue';
+    const counter = ref(0);
+
+    const increaseCounter = () => {counter.value++;};
+    const decreaseCounter = ()=> {counter.value--;};
+</script>
+
+<!-- setup funtion pattern -->
+<!-- <script>
+import { ref } from 'vue';
+  export default {
+    setup(){
+      const counter = ref(0);
+
+      const increaseCounter = () => {counter.value++;};
+      const decreaseCounter = ()=> {counter.value--;};
+      return {
+        counter,
+        increaseCounter,
+        decreaseCounter
+      };
+    }
+  }
+</script> -->
+
+<!-- option api-->
+<!-- <script>
+export default {
+  data(){
+    return {
+      counter : 0
+    }
+  },
+  methods: {
+    increaseCounter(){
+      this.counter++;
+    },
+    decreaseCounter(){
+      this.counter--;
+    }
+  }
+}
+
+</script> -->
+<style>
+.home{
+  text-align: center; 
+  padding: 20px;
+}
+.counter, .btn {
+  font-size: 40px;
+}
+</style>
