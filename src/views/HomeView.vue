c638c570aad3f26905297e7e4529292dc79eb29d
mohammad sadegh ilaghi <m.hoseini966@gmail.com>
2023-03-14 10:13:02 +0330

    learning computed and pass parameter.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,13 @@
 <template>
   <h3>{{ countData.title  }}</h3>
   <div class="home">
-    <button @click="decreaseCounter" class="btn">-</button> 
+    <button @click="decreaseCounter(1, $event)" class="btn">-</button> 
+    <button @click="decreaseCounter(2, $event)" class="btn">--</button> 
     <span class="counter">{{ countData.count }}</span>   
-    <button @click="increaseCounter" class="btn">+ </button>
+    <button @click="increaseCounter(2, $event)" class="btn">++ </button>
+    <button @click="increaseCounter(1, $event)" class="btn">+ </button>
   </div>
-
+  <p>this number is {{ oddOrEven }}</p>
   <div class="edit">
     <h4>edit Counter title</h4>
     <input v-model="countData.title" type="text" >
@@ -15,23 +17,28 @@
 
 <!-- script setup pattern -->
 <script setup>
-import { ref, reactive } from 'vue';
+import { ref, reactive, computed } from 'vue';
 
     // ref data
-    const counter = ref(0), 
-           counterTitle = ref('My Counter');
+    const counter_noneReactive = ref(0), 
+           counterTitle_noneReactive = ref('My Counter');
     // reactive data
     const countData = reactive({
       count: 0,
       title: 'My Counter'
     });
 
+    // computed in vue
+    const oddOrEven = computed(()=>{
+      if(countData.count % 2 == 0) return "odd";
+      return "even";
+    });
     // non reactive data 
     const counter = 0, counterTitle = "my counter";
 
     // methods
-    const increaseCounter = () => {countData.count++;};
-    const decreaseCounter = ()=> {countData.count--;};
+    const increaseCounter = (amount, e) => {countData.count += amount;};
+    const decreaseCounter = (amount, e)=> {countData.count -= amount;};
 </script>
 
 <!-- setup funtion pattern -->
